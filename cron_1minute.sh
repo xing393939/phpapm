8038a18e6c91e980bd980db346330407c97dbd8e
chenxing <xing393939@qq.com>
2015-01-30 11:29:44 +0800

    phpapm

--- cron_1minute.sh
+++ cron_1minute.sh
@@ -5,7 +5,7 @@ cd $project_pwd/phpapm
 
 callact "/alidata/server/php/bin/php crontab.php act=monitor_fix"
 callact "/alidata/server/php/bin/php crontab.php act=file_change"
-callact2 "/alidata/server/php/bin/php crontab.php act=monitor"
+callact2 "/alidata/server/php/bin/php crontab.php act=monitor go=1"
 #callact2 "/alidata/server/php/bin/php crontab.php act=sysload"
 if [ 1 -eq $1 ] ; then
     callact2 "/alidata/server/php/bin/php crontab.php act=monitor_config del=1 master=yes"
