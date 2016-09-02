bb090896e48ea148f639d5529fa9ecf4c495c26b
guest <guest@guest.com>
2016-09-02 17:49:15 +0800

    更新建表sql，加索引，加注释

--- phpapm/include/phpapm.sql
+++ phpapm/include/phpapm.sql
@@ -7,7 +7,7 @@ CREATE TABLE `phpapm_monitor` (
   `V5` varchar(200) DEFAULT NULL,
   `FUN_COUNT` int(11) unsigned DEFAULT '0',
   `CAL_DATE` datetime DEFAULT NULL,
-  `V6` decimal(12,6) DEFAULT NULL,
+  `V6` decimal(12,6) DEFAULT NULL COMMENT '最大耗时',
   `MD5` char(32) DEFAULT NULL COMMENT '唯一标志',
   `ADD_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `TOTAL_DIFF_TIME` decimal(12,6) DEFAULT NULL COMMENT '花费总耗时',
@@ -19,7 +19,8 @@ CREATE TABLE `phpapm_monitor` (
   `CPU_SYS_TIME_TOTAL` decimal(12,6) DEFAULT NULL COMMENT '系统消耗CPU,总',
   `OCI_UNIQUE` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '模拟ocirowcount',
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `MD5` (`MD5`)
+  UNIQUE KEY `MD5` (`MD5`),
+  KEY `CAL_DATE` (`CAL_DATE`)
 ) ENGINE=InnoDB AUTO_INCREMENT=53375 DEFAULT CHARSET=utf8 COMMENT='所有监控';
 
 CREATE TABLE `phpapm_monitor_config` (
@@ -27,20 +28,20 @@ CREATE TABLE `phpapm_monitor_config` (
   `V1` varchar(100) DEFAULT NULL,
   `V2` varchar(100) DEFAULT NULL,
   `COUNT_TYPE` varchar(10) DEFAULT NULL COMMENT '日综合数据的统计方式',
-  `V3_LINK` varchar(200) DEFAULT NULL COMMENT 'V3的连接方\r\n式',
-  `V4_LINK` varchar(200) DEFAULT NULL COMMENT 'V4的连接\r\n方式',
+  `V3_LINK` varchar(200) DEFAULT NULL COMMENT 'V3的连接方式',
+  `V4_LINK` varchar(200) DEFAULT NULL COMMENT 'V4的连接方式',
   `ORDERBY` decimal(4,0) DEFAULT NULL,
-  `PHONE` varchar(100) DEFAULT NULL COMMENT '手机号码,\r\n分号隔开',
+  `PHONE` varchar(100) DEFAULT NULL COMMENT '手机号码,分号隔开',
   `PHONE_ORDER` decimal(10,0) DEFAULT NULL COMMENT '手机通知条件大于',
   `PHONE_ORDER_LESS` decimal(10,0) DEFAULT NULL COMMENT '手机通知条件小于',
-  `AS_NAME` varchar(100) DEFAULT NULL COMMENT '页\r\n面上面显示的别名',
+  `AS_NAME` varchar(100) DEFAULT NULL COMMENT '页面上面显示的别名',
   `DAY_COUNT_TYPE` varchar(10) NOT NULL DEFAULT '0' COMMENT '日数据的统计方式:',
   `HOUR_COUNT_TYPE` varchar(10) NOT NULL DEFAULT '0' COMMENT '小时数据的统计方式:',
   `MIN_COUNT_TYPE` varchar(10) NOT NULL DEFAULT '0' COMMENT '分钟数据的统计方式:',
   `PERCENT_COUNT_TYPE` varchar(10) NOT NULL DEFAULT '0' COMMENT '是否显示百分比',
-  `V2_GROUP` varchar(100) DEFAULT NULL COMMENT 'v2分组名\r\n称',
+  `V2_GROUP` varchar(100) DEFAULT NULL COMMENT 'v2分组名称',
   `V2_COMPARE` decimal(4,0) DEFAULT '0' COMMENT '0为不显示，1为显示',
-  `PINFEN_RULE` text COMMENT '评分规则\r\n',
+  `PINFEN_RULE` text COMMENT '评分规则',
   `V2_CONFIG_OTHER` varchar(200) DEFAULT NULL COMMENT '序列化存储V2的其他属性,{NO_COUNT:true/false#标识数据是否需要统计入总数}',
   `COMPARE_GROUP` varchar(100) DEFAULT NULL,
   `VIRTUAL_COLUMNS` decimal(2,0) DEFAULT NULL,
@@ -74,7 +75,8 @@ CREATE TABLE `phpapm_monitor_hour` (
   `CPU_SYS_TIME_MAX` decimal(12,6) DEFAULT NULL COMMENT '系统消耗CPU,单次最大',
   `CPU_SYS_TIME_TOTAL` decimal(12,6) DEFAULT NULL COMMENT '系统消耗CPU,总',
   `OCI_UNIQUE` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '模拟ocirowcount',
-  PRIMARY KEY (`CAL_DATE`,`V1`,`V2`,`V3`)
+  PRIMARY KEY (`CAL_DATE`,`V1`,`V2`,`V3`),
+  UNIQUE KEY `V1` (`V1`,`V2`,`V3`,`CAL_DATE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `phpapm_monitor_queue` (
@@ -85,8 +87,8 @@ CREATE TABLE `phpapm_monitor_queue` (
 
 CREATE TABLE `phpapm_monitor_v1` (
   `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `V1` varchar(100) NOT NULL COMMENT '\r\n名称',
-  `AS_NAME` varchar(100) DEFAULT NULL COMMENT '页面上\r\n面显示的别名',
+  `V1` varchar(100) NOT NULL COMMENT '名称',
+  `AS_NAME` varchar(100) DEFAULT NULL COMMENT '页面上面显示的别名',
   `COUNT_TYPE` varchar(10) DEFAULT NULL COMMENT '日综合数据的统计方式',
   `CHAR_TYPE` decimal(10,0) DEFAULT NULL COMMENT '默认显示的图标类型',
   `GROUP_NAME` varchar(100) NOT NULL DEFAULT '默认' COMMENT '分组名称',
